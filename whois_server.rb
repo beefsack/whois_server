4c0c7935ee4813edb0a7ed575d432d38aeb5c3a4
Michael Alexander <beefsack@gmail.com>
2011-12-05 18:08:43 +1100

    Implemented all values given from whois library.

--- whois_server.rb
+++ whois_server.rb
@@ -3,12 +3,34 @@ require 'sinatra'
 require 'whois'
 require 'json'
 
+def hash_or_nil(value)
+  value ? Hash[*value.members.zip(value.values).flatten] : nil
+end
+
 get '/:domain' do
   content_type :json
   c = Whois::Client.new
   result = c.query(params[:domain])
   data = {
+    :disclaimer => result.disclaimer,
+    :domain => result.domain,
+    :domain_id => result.domain_id,
+    :referral_whois => result.referral_whois,
+    :status => result.status,
+    :registered => result.registered?,
     :available => result.available?,
+    :created_on => result.created_on,
+    :updated_on => result.updated_on,
+    :expires_on => result.expires_on,
+    :nameservers => result.nameservers,
     }
+  data[:registrar] = hash_or_nil(result.registrar)
+  data[:registrant_contact] = hash_or_nil(result.registrant_contact)
+  data[:admin_contact] = hash_or_nil(result.admin_contact)
+  data[:technical_contact] = hash_or_nil(result.technical_contact)
   data.to_json
+end
+
+get "/" do
+  "Add a domain to the URL to check.  For example: http://#{request.host}/google.com"
 end
